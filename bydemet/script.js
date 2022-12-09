6c11115ec911b7cdd2f0bdd60d14f0278b1f5d9e
nra95 <noreskelund@gmail.com>
2022-12-09 10:25:38 +0100

    4

--- bydemet/script.js
+++ bydemet/script.js
@@ -1,1 +1,16 @@
+AOS.init();
+
+import AOS from 'aos';
+
+window.addEventListener("DOMContentLoaded", () => {
+
+const hero = document.getElementById(tidcent);
+hero.setAttribute('data-aos', 'fade-up');
+
+    AOS.init({
+        initClassName: "aos-init",
+        startEvent: "DOMContentLoaded",
+    });
+});
+
 document.body.className = "visible";
