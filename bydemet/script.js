c86aead9dba08b468d1f3cd396bfaa8049d880bd
Mettha <mettegrau@hotmail.com>
2022-12-09 09:27:53 +0100

    faq vs2

--- bydemet/script.js
+++ bydemet/script.js
@@ -1,1 +1,16 @@
 document.body.className = "visible";
+
+//FAQ side onclick event
+var tag = document.getElementById('svar1');
+var button = document.querySelector('button');
+button.addEventListener('click', function(){
+tag.classList.toggle('Svar');
+});
+
+
+
+
+
+
+
+
