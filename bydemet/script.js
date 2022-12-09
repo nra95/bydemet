52f0f239bdf1ae99d07a955ce2737a5f0dcb8531
Mettha <mettegrau@hotmail.com>
2022-12-09 11:50:20 +0100

    faq side med javascript

--- bydemet/script.js
+++ bydemet/script.js
@@ -1,15 +1,24 @@
 document.body.className = "visible";
 
-//FAQ side onclick event
-var tag = document.getElementById('svar1');
-var button = document.querySelector('button');
-button.addEventListener('click', function(){
-tag.classList.toggle('Svar');
-});
+//FAQ side eventlistner + for løkke
+var faq = document.getElementsByClassName("faq-spm");
+var i;
 
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+       /* Toggler mellem tilføje og remove "active" class,
+        for at vise arrow up og down knapperne */
+        this.classList.toggle("active");
 
-
-
+        /* Toggler mellem at skjule og vise svar panel */
+        var svar = this.nextElementSibling;
+        if (svar.style.display === "block") {
+            svar.style.display = "none";
+        } else {
+            svar.style.display = "block";
+        }
+    });
+}
 
 
 
