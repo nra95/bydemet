7fcc02641eba22143dbfe029eedaf5f652ec411f
nra95 <noreskelund@gmail.com>
2022-12-13 10:00:10 +0100

    3

--- bydemet/script.js
+++ bydemet/script.js
@@ -1,20 +1,3 @@
-AOS.init();
-
-import AOS from 'aos';
-
-window.addEventListener("DOMContentLoaded", () => {
-
-const hero = document.getElementById(tidcent);
-hero.setAttribute('data-aos', 'fade-up');
-
-    AOS.init({
-        initClassName: "aos-init",
-        startEvent: "DOMContentLoaded",
-    });
-});
-
-document.body.className = "visible";
-
 //FAQ side eventlistner + for løkke
 var faq = document.getElementsByClassName("faq-spm");
 var i;
