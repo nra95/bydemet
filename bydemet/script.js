b73b4e669f515c451b264853cc2aecb47f7c9030
nra95 <noreskelund@gmail.com>
2022-12-14 12:25:36 +0100

    5

--- bydemet/script.js
+++ bydemet/script.js
@@ -14,6 +14,28 @@ $(window).scroll(function() {
         showImages('#follow2');
 });
 
+
+/*Automatisk slideshow på behandlings siden*/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
 //FAQ side eventlistner + for løkke
 var faq = document.getElementsByClassName("faq-spm");
 var i;
